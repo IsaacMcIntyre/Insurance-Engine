81cb3ea2f34ac8fd34ec60a9c62a416d0d6c905b
Isaac McIntyre <isaacmmcintyre@gmail.com>
2019-10-18 16:22:52 +0100

    First commit.

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,31 @@
+describe('Personal calculations come before Vehicle calculations.', () => {
+  //find a way to fail it with personal calculations
+});
+
+test('Occupation of Stunt Driver adds £200 onto premium', () => {
+  //occupation test
+});
+
+test('Occupation of Sandwich Artist reduces premium by 10%', () => {
+  //occupation test
+});
+
+test('City of London must be declined with a reason of “LDN High Risk”', () => {
+  //send in city of london as a risk and then get a decline 
+});
+
+test('If person calculations detect a decline then do not execute vehicle calculations', () => {
+  //
+});
+
+test('Vehiles withocut security device should Report with reason of “SDEV MISS”', () => {
+
+});
+
+test('Vehicles over 10 years old increases premium by 5% for each year over 10', () => {
+
+});
+
+test('All responses must return a result of Accept, Report or Decline', () => {
+
+});
---
+++ src/index.js
@@ -0,0 +1,31 @@
+calculate = (risks) => {
+  let premium = 0;
+  response = personal(premium, personalRisks)
+}
+
+personal = (premium, personalRisks) => {
+  //occupation
+  //city
+}
+
+vehicle = (premium, vehicleRisks) => {
+  //has security device
+  //manufacture date
+  //
+}
+
+occupation = (premium, occupation) => {
+
+}
+
+city = (premium, city) => {
+
+}
+
+securityDeviceExists = (premium, exists) => {
+
+}
+
+manufactureDate = (premium, date) => {
+
+}
