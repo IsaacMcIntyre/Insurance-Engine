c5a4840c1a5643a3f2fa86afc70fedcb8e9fd03a
Isaac McIntyre <isaacmcintyre@Isaacs-MacBook-Pro.local>
2019-10-24 14:38:12 +0100

    Further changes.

--- src/__tests__/vehicle.test.js
+++ src/__tests__/vehicle.test.js
@@ -1,9 +1,13 @@
+const vehicle = require('../vehicle');
+
 describe('vehicle', () => {
   test('Vehiles without security device should Report with reason of “SDEV MISS”', () => {
-  
+    const response = vehicle({ "model": "Ford", "manufacturedDate": "2019-01-01", "colour": "Red", "hasSecurityDevice": true }, 500);
+    expect(response.reason).toBe('SDEV MISS');
   });
-  
+
   test('Vehicles over 10 years old increases premium by 5% for each year over 10', () => {
-  
+    const response = vehicle({ "model": "Ford", "manufacturedDate": "2008-01-01", "colour": "Red", "hasSecurityDevice": true }, 500);
+    expect(response.premium).toBe('');
   });
 });
--- src/vehicle.js
+++ src/vehicle.js
@@ -1,4 +1,6 @@
 module.exports = (vehicle, premium) => {
+  let response = { result: null, reason: null, premium: premium };
 
+  return response;
 }
 
